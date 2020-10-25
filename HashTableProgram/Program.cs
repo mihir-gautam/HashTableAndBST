8d78a6bd5d9c8a6045df26a4a410e45364198def
Mihir Gautam <Mihir Gautam@DESKTOP-JPP42H1>
2020-10-25 14:09:47 +0530

    [Mihir] Add. Welcome Message

---
+++ HashTableProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HashTableProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Hash Table Data structure practice problem.");
+        }
+    }
+}
